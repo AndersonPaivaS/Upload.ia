28fc23cff848802ef87227511843a652a692a402
Your Name <you@example.com>
2023-09-18 20:30:33 -0300

    feat: install fastify Cors

--- upload-ia-api/src/server.ts
+++ upload-ia-api/src/server.ts
@@ -3,12 +3,19 @@ import { prisma } from './lib/prisma'
 import { getAllPromptsRoute} from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { GenerateAICompletionRoute } from './routes/generate-ia-completion'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(GenerateAICompletionRoute)
 
 app.listen({
     port: 3333
